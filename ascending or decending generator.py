1617f70df5f9682c23950575f1a2152de48a87f3
dhirajms <msdhiraj333@gmail.com>
2025-01-10 16:39:11 +0530

    Add files via upload

---
+++ ascending or decending generator.py
@@ -0,0 +1,16 @@
+while True:
+    a=input("enter your number:-  ")
+    b=input("enter your number:-  ")
+    c=input("enter your number:-  ")
+    d=input("enter your number:-  ")
+    x=input("ascending or decending:-  ")
+    if x== 'ascending':
+        
+       list=[a,b,c,d]
+       list.sort()
+       print('ans:-',list)
+
+    else:
+        list=[a.b.c.d]
+        list.sort(reverse=True)
+        print(list)
